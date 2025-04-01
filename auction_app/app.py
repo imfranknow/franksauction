b4b7344ad1811949de0475b2c5167c2e9b22ee98
imfranknow <oliverbjorgan@gmail.com>
2025-04-01 17:57:47 +0200

    Update app.py

--- auction_app/app.py
+++ auction_app/app.py
@@ -90,4 +90,7 @@ def auction():
                            bids=bids)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
